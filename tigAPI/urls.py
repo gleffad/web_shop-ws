88cb3807a36e97a69c1bf94329909cebae6b6bd9
Melimelo94400 <irles.kevin@gmail.com>
2021-04-21 15:21:59 +0200

    order routes

--- tigAPI/urls.py
+++ tigAPI/urls.py
@@ -3,13 +3,13 @@ from tigAPI import views
 
 urlpatterns = [
     path('products/', views.ProductList.as_view()),
+    path('products/fish', views.ProductFish.as_view()),
+    path('products/shellfish', views.ProductShellfish.as_view()),
+    path('products/seafood', views.ProductSeafood.as_view()),
     path('product/<int:tigID>/', views.Product.as_view()),
     path('transactions/', views.TransactionList.as_view()),
     path('transaction/<int:tigID>/', views.Transaction.as_view()),
     path('setdiscount/<int:tigID>/<int:discount>/', views.SetDiscount.as_view()),
-    path('products/fish', views.ProductFish.as_view()),
-    path('products/shellfish', views.ProductShellfish.as_view()),
-    path('products/seafood', views.ProductSeafood.as_view()),
     path('decrementstock/<int:tigID>/<int:qty>/<int:operation>/',
          views.DecrementStock.as_view()),
     path('incrementstock/<int:tigID>/<int:qty>/',
